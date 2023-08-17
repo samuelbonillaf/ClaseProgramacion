36c90646701600a9870412678607a680ebfe691f
samuelbonillaf <141078530+samuelbonillaf@users.noreply.github.com>
2023-08-17 00:01:51 +0000

    Def

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-"""
-N = int(input("Ingrese un numero"))
-M = int(input("Ingrese un numero"))
-SU=M/N
-print(round(SU, 3))
+def div (N1, N2):
+  if N2!=0:
+    Res=N1/N2
+    return Res
+  else:
+    print("No se divide por 0")
+    return 111
+def mul (N1, N2):
+  Res=N1*N2
+  return Res
+def sum (N1, N2):
+  Res=N1+N2
+  return Res
+def res (N1, N2):
+  Res=N1-N2
+  return Res
+  
+NumeroA=float(input("Numero 1:"))
+NumeroB=float(input("Numero 2:"))
 
-Inversion=float(input("Inversion:"))
-Interes=float(input("Interes:"))
-Tiempo=float(input("Tiempo en años"))
-Valor=Inversion*(Interes/100+1)**Tiempo
-print("Valor total", Valor)
-"""
-Payaso=int(input("Cuantos payasos:"))
-Muñecas=int(input("Cuantas muñecas:"))
-Peso=(Payaso*112)+(Muñecas*75)
-print("El paquete va a pesar: ", Peso)
+dec=input("Que operacion desea realizar: + para suma \n 2 para resta \n 3 para multiplicacion \n 4 para division")
+if dec==1:
+  sum(NumeroA, NumeroB)
+elif dec==2:
+  res(NumeroA, NumeroB)
+elif dec==3:
+  mul(NumeroA, NumeroB)
+elif dec==4:
+  div(NumeroA, NumeroB)
+else:
+  print("Esa no es una seleccion") 
+
+
+print(div(NumeroA, NumeroB))
